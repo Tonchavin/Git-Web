e07283ade4258ce086cac559b6dd5fea5c41796d
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-05-29 21:52:41 +1000

    add fix task3

---
+++ task3/1.js
@@ -0,0 +1,3 @@
+const celsius = Number.parseInt(prompt("Введите температиру в Цельсиях: "));
+const fahrenheit = Math.round((9 / 5) * celsius + 32);
+alert(`Цельсий: ${celsius}, Фаренгейт: ${fahrenheit}`);
---
+++ task3/2.js
@@ -0,0 +1,5 @@
+let name = prompt("Введите имя: ");
+greeting(`Приветствую: ${name}`);
+function greeting(in_name) {
+    console.log(in_name);
+}
